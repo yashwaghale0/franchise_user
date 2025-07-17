2c905fe5eedcf28c82b5850577abaa9a6a4390ea
tejas-sat <tejass@internetsoft.com>
2025-07-17 20:09:21 +0530

    user dashboard created

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
 export const PORT = 5000;
-// export const BASE_URL = "https://dev.franchiselistings.com/franchise_backend";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://dev.franchiselistings.com/franchise_backend";
+// export const BASE_URL = "http://localhost:5000";
