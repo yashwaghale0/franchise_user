66f74c704365a26827f2c9ab90e65ca32d3876e2
tejas-sat <tejass@internetsoft.com>
2025-08-07 15:21:58 +0530

    login issue solved

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://dev.franchiselistings.com/franchise_backend";
-export const BASE_URL = "http://localhost:5000";
-//export const BACKEND_URL =
-// "https://dev.franchiselistings.com/franchise_backend";
-export const BACKEND_URL = "http://localhost:5000";
+export const BASE_URL = "https://dev.franchiselistings.com/franchise_backend";
+// export const BASE_URL = "http://localhost:5000";
+export const BACKEND_URL =
+  "https://dev.franchiselistings.com/franchise_backend";
+// export const BACKEND_URL = "http://localhost:5000";
